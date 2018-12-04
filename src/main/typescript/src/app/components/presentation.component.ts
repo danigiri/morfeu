c1bd1446aaec0b11b6931664918ae03c911f1a7c
Daniel Giribet <dani@calidos.cat>
2018-12-05 00:04:50 +0100

    Some baby steps to reuse html presentation

--- src/main/typescript/src/app/components/presentation.component.ts
+++ src/main/typescript/src/app/components/presentation.component.ts
@@ -9,60 +9,16 @@ import { CellModel } from "../cell-model.class";
 	moduleId: module.id,
 	selector: "presentation",
 	template: `
-		<ng-container *ngIf="cell && draggable">
-			<img *ngIf="cellPresentationIsIMG()"
-				id="{{cell.URI}}"
-				class="cell cell-img cell-level-{{level}}"
-				src="{{getCellPresentation()}}"
-				[class.cell-active]="active"
-				[class.cell-selected]="selected"
-				(mouseenter)="focusOn(cell)"
-				(mouseleave)="focusOff(cell)"
-				(dblclick)="doubleClick()"
-				dnd-draggable
-				[dragEnabled]="dragEnabled"
-				(onDragEnd)="dragEnd(cell)"
-				[dragData]="cellDragData()"
-			/>
-			<!-- TODO: add innerhtml type? -->
-			<iframe *ngIf="!cellPresentationIsIMG()" 
-				id="{{cell.URI}}"
-				[src]="getCellPresentation() | safe: 'resourceUrl'"
-				[class.cell-active]="active"
-				[class.cell-selected]="selected"
-				(mouseenter)="focusOn(cell)"
-				(mouseleave)="focusOff(cell)"
-				(dblclick)="doubleClick()"
-				dnd-draggable
-				[dragEnabled]="dragEnabled"
-				(onDragEnd)="dragEnd(cell)"
-				[dragData]="cellDragData()"
-			></iframe>
-
-		</ng-container>
-		<ng-container *ngIf="cellModel && !draggable">
-		</ng-container>
+		<!-- TODO: add inner html type? -->
+		<iframe *ngIf="!cellPresentationIsIMG()" 
+					class="cell cell-html"
+					[src]="getCellPresentation() | safe: 'resourceUrl'"
+					[class.cell-active]="active"
+					[class.cell-selected]="selected"
+		></iframe>
 	`,
 	styles:[`
-		.cell-level-0 {}
-		.cell-level-1 {}
-		.cell-level-2 {}
-		.cell-level-3 {}
-		.cell-level-4 {}
-		.cell-level-5 {}
-		.cell-level-6 {}
-		.cell-level-7 {}
-		.cell-level-8 {}
-		.cell-level-9 {}
-		.cell-level-10 {}
-		.cell-level-11 {}
-		.cell-level-12 {}
-		.cell-level-13 {}
-		.cell-level-14 {}
-		.cell-level-15 {}
-		.cell-level-16 {}
-		.cell-level-17 {}
-		.cell-level-18 {}
+
 	`]
 })
 
