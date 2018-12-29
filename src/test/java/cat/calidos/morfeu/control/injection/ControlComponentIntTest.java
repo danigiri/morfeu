560b82e15272708a1efe6585509212d2bf8af951
Daniel Giribet <dani@calidos.cat>
2018-12-29 20:34:25 +0100

    Some testing for context

--- src/test/java/cat/calidos/morfeu/control/injection/ControlComponentIntTest.java
+++ src/test/java/cat/calidos/morfeu/control/injection/ControlComponentIntTest.java
@@ -1,3 +1,106 @@
+// CONTROL COMPONENT INT TEST . JAVA
+
+package cat.calidos.morfeu.control.injection;
+
+import static org.junit.Assert.*;
+
+import static org.mockito.Mockito.*;
+
+import java.util.HashMap;
+
+import javax.servlet.ServletContext;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+import cat.calidos.morfeu.webapp.injection.ControlComponent;
+import cat.calidos.morfeu.webapp.injection.DaggerControlComponent;
+
+/**
+* @author daniel giribet
+*///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+public class ControlComponentIntTest {
+
+@Rule public MockitoRule mockitoRule = MockitoJUnit.rule(); 
+@Mock ServletContext context;
+
+HashMap<String, String> emptyParams = new HashMap<String, String>(0);
+
+
+@Test
+public void testPingControl() {
+
+	ControlComponent controlComponent = DaggerControlComponent.builder()
+																.withPath("/ping")
+																.method(DaggerControlComponent.GET)
+																.withParams(emptyParams)
+																.andContext(context)
+																.build();
+	assertTrue("Should match /ping path", controlComponent.matches());
+	assertEquals("OK", controlComponent.process());
+	assertEquals(DaggerControlComponent.TEXT, controlComponent.contentType());
+	
+}
+
+
+@Test
+public void testPingControlWithParam() {
+
+	ControlComponent controlComponent = DaggerControlComponent.builder()
+																.withPath("/ping/param")
+																.method(DaggerControlComponent.GET)
+																.withParams(emptyParams)
+																.andContext(context)
+																.build();
+	assertTrue("Should match /ping/param path", controlComponent.matches());
+	assertEquals("OK param", controlComponent.process());
+	assertEquals(DaggerControlComponent.TEXT, controlComponent.contentType());
+
+}
+
+
+@Test
+public void testNoMatch() {
+
+	ControlComponent controlComponent = DaggerControlComponent.builder()
+																.withPath("/foo")
+																.method(DaggerControlComponent.GET)
+																.withParams(emptyParams)
+																.andContext(context)
+																.build();
+
+	assertFalse(controlComponent.matches());
+	assertEquals(DaggerControlComponent.TEXT, controlComponent.contentType());
+
+}
+
+
+@Test
+public void testContext() {
+
+	when(context.getAttribute("counter")).thenReturn(0);
+
+	ControlComponent controlComponent = DaggerControlComponent.builder()
+																.withPath("/counter")
+																.method(DaggerControlComponent.GET)
+																.withParams(emptyParams)
+																.andContext(context)
+																.build();
+	assertTrue("Should match /counter path", controlComponent.matches());
+	assertEquals("1", controlComponent.process());
+	assertEquals(DaggerControlComponent.TEXT, controlComponent.contentType());
+
+}
+
+
+}
+
 /*
  *    Copyright 2018 Daniel Giribet
  *
@@ -13,65 +116,3 @@
  *   See the License for the specific language governing permissions and
  *   limitations under the License.
  */
-
-package cat.calidos.morfeu.control.injection;
-
-import static org.junit.Assert.*;
-
-import java.util.HashMap;
-
-import org.junit.Test;
-
-import cat.calidos.morfeu.webapp.injection.ControlComponent;
-import cat.calidos.morfeu.webapp.injection.DaggerControlComponent;
-
-/**
-* @author daniel giribet
-*///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-public class ControlComponentIntTest {
-
-@Test
-public void testPingControl() {
-
-	ControlComponent controlComponent = DaggerControlComponent.builder()
-																.withPath("/ping")
-																.method(DaggerControlComponent.GET)
-																.withParams(new HashMap<String, String>(0))
-																.build();
-	assertTrue("Should match /ping path", controlComponent.matches());
-	assertEquals("OK", controlComponent.process());
-	assertEquals(DaggerControlComponent.TEXT, controlComponent.contentType());
-	
-}
-
-
-@Test
-public void testPingControlWithParam() {
-
-	ControlComponent controlComponent = DaggerControlComponent.builder()
-																.withPath("/ping/param")
-																.method(DaggerControlComponent.GET)
-																.withParams(new HashMap<String, String>(0))
-																.build();
-	assertTrue("Should match /ping/param path", controlComponent.matches());
-	assertEquals("OK param", controlComponent.process());
-	assertEquals(DaggerControlComponent.TEXT, controlComponent.contentType());
-
-}
-
-
-@Test
-public void testNoMatch() {
-	
-	ControlComponent controlComponent = DaggerControlComponent.builder()
-																.withPath("/foo")
-																.method(DaggerControlComponent.GET)
-																.withParams(new HashMap<String, String>(0))
-																.build();
-
-	assertFalse(controlComponent.matches());
-	assertEquals(DaggerControlComponent.TEXT, controlComponent.contentType());
-
-}
-
-}
