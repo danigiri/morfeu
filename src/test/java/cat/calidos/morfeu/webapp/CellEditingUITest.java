affa8b0e8d5cef5111c3cb24c9116aaf3b6b454b
Daniel Giribet <dani@calidos.cat>
2019-01-20 23:50:23 +0100

    Fixed editing tests

--- src/test/java/cat/calidos/morfeu/webapp/CellEditingUITest.java
+++ src/test/java/cat/calidos/morfeu/webapp/CellEditingUITest.java
@@ -1,18 +1,4 @@
-/*
- *    Copyright 2018 Daniel Giribet
- *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
- */
+// CELL EDITING UI TEST . JAVA
 
 package cat.calidos.morfeu.webapp;
 
@@ -57,7 +43,7 @@ public void setup() {
 
 @Test
 public void editCellAndSave() {
-	
+
 	UICell data = test.child("row(0)").child("col(0)").child("data(0)").select().activate();
 	assertNotNull(data);
 	UICellEditor.shouldNotBeVisible();
@@ -67,17 +53,17 @@ public void editCellAndSave() {
 
 	UICellEditor dataEditor = data.edit().shouldAppear();
 	assertNotNull(dataEditor);
-	
+
 	UICellData cellEditorData = dataEditor.cellData();
 	assertNotNull(cellEditorData);
 	assertTrue("Editing the cell should show an editor", cellEditorData.isFromEditor());
 	assertTrue("Editing the cell should show an editor with data coming from the cell", cellEditorData.isFromCell());
-	
+
 	attributes = cellEditorData.attributes();
 	assertNotNull(attributes);
 	assertEquals("We should be editing two attributes", 2, attributes.size());
-	UIAttributeData name = checkAttribute(attributes, "text", "blahblah");
-	assertTrue("Attribute text should be editable", name.isEditable());
+	UIAttributeData text = checkAttribute(attributes, "text", "blahblah");
+	assertTrue("Attribute text should be editable", text.isEditable());
 
 	Optional<UIAttributeData> numberOpt = attributes.stream().filter(a -> a.name().matches("number")).findFirst();
 	assertTrue(numberOpt.isPresent());
@@ -85,12 +71,13 @@ public void editCellAndSave() {
 	assertTrue("Attribute number should be editable", number.isEditable());
 
 	// let's modify the values
-	name.enterTextDirect("foo");
+	text.eraseValueInField();
+	text.enterTextDirect("foo");
 	number.enterTextNext("66");
 	dataEditor.clickSave();
 
 	attributes = data.cellInfo().attributes();	// re-read attributes from the now-modified cell
-	name = checkAttribute(attributes, "text", "foo");
+	text = checkAttribute(attributes, "text", "foo");
 	number = checkAttribute(attributes, "number", "66");
 
 }
@@ -98,7 +85,7 @@ public void editCellAndSave() {
 
 @Test
 public void editCellAndDismiss() {
-	
+
 	UICell data = test.child("row(0)").child("col(0)").child("data(0)").select().activate();
 	assertNotNull(data);
 	List<UIAttributeData> attributes = data.cellInfo().attributes();
@@ -106,29 +93,30 @@ public void editCellAndDismiss() {
 	checkAttribute(attributes, "number", "42");
 
 	UICellEditor.shouldNotBeVisible();
-	
+
 	UICellEditor dataEditor = data.edit();
 	assertNotNull(dataEditor);
 	UICellData cellEditorData = dataEditor.shouldAppear().cellData();
 	assertNotNull(cellEditorData);
-	
+
 	attributes = cellEditorData.attributes();
 	assertNotNull(attributes);
-	UIAttributeData name = checkAttribute(attributes, "text", "blahblah");
+	UIAttributeData text = checkAttribute(attributes, "text", "blahblah");
 	UIAttributeData number = checkAttribute(attributes, "number", "42");
-	
-	// let's modify the values
-	name.enterTextDirect("foo");
-	number.enterTextDirect("66");
-	
+
+	// let's modify the values (this is brittle as it needs to be done in order
+	text.eraseValueInField();
+	text.enterTextDirect("foo");
+	number.enterTextNext("66");
+
 	dataEditor.clickDiscard();
 
 	data.select();
 	data.activate();
 	attributes = data.cellInfo().attributes();				// re-read attributes from the now-modified cell
-	name = checkAttribute(attributes, "text", "blahblah");		// boom, magically restored
+	text = checkAttribute(attributes, "text", "blahblah");		// boom, magically restored
 	number = checkAttribute(attributes, "number", "42");
-	
+
 }
 
 
@@ -166,3 +154,19 @@ private UIAttributeData checkAttribute(List<UIAttributeData> attributes, String 
 
 
 }
+
+/*
+ *    Copyright 2019 Daniel Giribet
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
--- src/test/java/cat/calidos/morfeu/webapp/ui/UIAttributeData.java
+++ src/test/java/cat/calidos/morfeu/webapp/ui/UIAttributeData.java
@@ -1,18 +1,4 @@
-/*
- *    Copyright 2018 Daniel Giribet
- *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
- */
+// UI ATTRIBUTE DATA . JAVA
 
 package cat.calidos.morfeu.webapp.ui;
 
@@ -99,19 +85,20 @@ public UIAttributeData enterTextDirect(String value) {
 	if (!isEditable()) {
 		throw new UnsupportedOperationException("Cannot set the value of a non-editable field");
 	}
-	
+
 	int attributeIndex = Integer.parseInt(element.attr("_index"));
-	
-	for (int i=0; i<=attributeIndex; i++) {
+	for (int i=0; i<attributeIndex; i++) {	// it seems that we have the cursor on the first data field for some reason
 		pressKey("\t");
 	}
+
 	// this only works if called in order, selenide is not happy about the setValue so we're hacking it
 	pressKey(value);
-	
+
 	//element.$(".attribute-data-value").setValue(value);
 	//element.setValue(value);
-	
+
 	return this;
+
 }
 
 
@@ -128,17 +115,51 @@ public UIAttributeData enterTextNext(String value) {
 }
 
 
+/** Erase the attribute text in the field, we assume our cursor is in the field  */
+public UIAttributeData eraseValueInField() {
+
+	if (!isEditable()) {
+		throw new UnsupportedOperationException("Cannot erase the value of a non-editable field");
+	}
+
+
+	int length = value().length();
+	for (int i=0;i<length;i++) {
+		pressBackspace();
+	}
+
+	return this;
+
+}
+
+
 public UIAttributeData clickOnCreate() {
 
 	if (!isNotPresent()) {
 		throw new UnsupportedOperationException("Cannot create an attribute unless it's not present");
 	}
-	
+
 	element.$(".attribute-data-add").click();	// we click on the add button
-	
+
 	return parent.attribute(this.name());		// and we now have new element, so we recreate ourselves
-	
+
 }
 
 
 }
+
+/*
+ *    Copyright 2019 Daniel Giribet
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
