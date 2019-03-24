ab41c270c970ede4cb803912d2741e9903e6f524
Daniel Giribet <dani@calidos.cat>
2019-03-24 18:50:51 +0100

    All non-UI integration tests pass now

--- src/test/java/cat/calidos/morfeu/model/transform/TransformIdentifierIntTest.java
+++ src/test/java/cat/calidos/morfeu/model/transform/TransformIdentifierIntTest.java
@@ -1,18 +1,4 @@
-/*
- *    Copyright 2018 Daniel Giribet
- *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
- */
+// TRANSFORM IDENTIFIER INT TEST . JAVA
 
 package cat.calidos.morfeu.model.transform;
 
@@ -46,14 +32,14 @@ public void testTransformJSONToYAML() throws Exception {
 	assertNotNull(doc);
 
 	Map<String, Object> values = new HashMap<String, Object>(2);
-	values.put("cells", doc.children().asList());
+	values.put("cells", doc.children().child(0).asComplex().children().asList()); // skip root node
 	values.put("model", doc.getModel());
 
 	String transformed = DaggerViewComponent.builder()
-			.withTemplatePath("templates/transform/content-to-yaml.twig")
-			.withValue(values)
-			.build()
-			.render();
+												.withTemplatePath("templates/transform/content-to-yaml.twig")
+												.withValue(values)
+												.build()
+												.render();
 	System.err.println(transformed);
 
 	YAMLMapper mapper = new YAMLMapper();
@@ -89,4 +75,21 @@ public void testTransformJSONToYAML() throws Exception {
 
 }
 
+
 }
+
+/*
+ *    Copyright 2019 Daniel Giribet
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
