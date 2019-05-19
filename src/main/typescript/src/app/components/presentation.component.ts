f52232f322427eab1f3cdc0bcc426372f538c6ab
Daniel Giribet <dani@calidos.cat>
2019-05-20 00:56:16 +0200

    Playing with iframe manual updates

--- src/main/typescript/src/app/components/presentation.component.ts
+++ src/main/typescript/src/app/components/presentation.component.ts
@@ -1,6 +1,6 @@
 // PRESENTATION . COMPONENT . TS (NOT USED AT THE MOMENT)
 
-import { Component, Input } from "@angular/core";
+import { Component, Input, ChangeDetectorRef, OnDestroy, ChangeDetectionStrategy } from "@angular/core";
 
 import { Cell } from "../cell.class";
 import { CellModel } from "../cell-model.class";
@@ -14,18 +14,31 @@ import { CellModel } from "../cell-model.class";
 			class="cell cell-html"
 			[src]="getPresentation() | safe: 'resourceUrl'"
 		></iframe>
+		
 	`,
-	styles:[`
-
-	`]
 })
+// sets the ui to be too slow as the iframe blocks rendering
+//	changeDetection: ChangeDetectionStrategy.OnPush
 
-export class PresentationComponent {
+export class PresentationComponent implements OnDestroy {
+
+//private interval: NodeJS.Timer;
 
 // if showing a cell with values or we are showing a cellmodel
 @Input() cell?: Cell;
 @Input() cellModel?: CellModel;
 
+constructor(private ref: ChangeDetectorRef) {
+
+//	ref.detach();
+//	this.interval = setInterval(() => { this.ref.detectChanges(); }, 1000);
+	
+}
+
+
+ngOnDestroy() {
+//	clearInterval(this.interval);
+}
 
 /** do we present the cell with the default (IMG) presentation? */
 private cellPresentationIsIMG(): boolean {
