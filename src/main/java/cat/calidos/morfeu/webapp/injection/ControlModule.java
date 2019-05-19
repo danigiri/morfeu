6cb03a68e6370008260b9af7ed77fd7701c2230a
Daniel Giribet <dani@calidos.cat>
2019-05-19 21:22:20 +0200

    Control module can now trace matches

--- src/main/java/cat/calidos/morfeu/webapp/injection/ControlModule.java
+++ src/main/java/cat/calidos/morfeu/webapp/injection/ControlModule.java
@@ -1,18 +1,4 @@
-/*
- *    Copyright 2018 Daniel Giribet
- *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
- */
+// CONTROL MODULE . JAVA
 
 package cat.calidos.morfeu.webapp.injection;
 
@@ -104,7 +90,12 @@ Map<Pattern, BiFunction<List<String>, Map<String, String>, String>> compiledCont
 @Provides
 public static Optional<Pattern> matchedPathPattern(@Named("Path") String path, 
 									Map<Pattern, BiFunction<List<String>, Map<String, String>, String>> controls) {
-	return controls.keySet().stream().filter(p -> p.matcher(path).matches()).findFirst();
+	return controls.keySet()
+					.stream()
+					.peek(p -> log.trace("Matching {} against pattern {}", path, p.toString()))
+					.filter(p -> p.matcher(path).matches())
+					.peek(p -> log.trace("* Matched with {} *", p.toString()))
+					.findFirst();
 }
 
 
@@ -139,3 +130,19 @@ public static String contentTypeDefaultMap() {
 
 
 }
+
+/*
+ *    Copyright 2018 Daniel Giribet
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
