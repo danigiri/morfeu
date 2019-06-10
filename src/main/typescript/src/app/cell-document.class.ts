d938e064b67e23270bcd3b266c8d8e8552ebc4d0
Daniel Giribet <dani@calidos.cat>
2019-06-10 17:12:54 +0200

    CellDocument was missing description

--- src/main/typescript/src/app/cell-document.class.ts
+++ src/main/typescript/src/app/cell-document.class.ts
@@ -1,19 +1,4 @@
-/*
- *	  Copyright 2017 Daniel Giribet
- *
- *	 Licensed under the Apache License, Version 2.0 (the "License");
- *	 you may not use this file except in compliance with the License.
- *	 You may obtain a copy of the License at
- *
- *		 http://www.apache.org/licenses/LICENSE-2.0
- *
- *	 Unless required by applicable law or agreed to in writing, software
- *	 distributed under the License is distributed on an "AS IS" BASIS,
- *	 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *	 See the License for the specific language governing permissions and
- *	 limitations under the License.
- */
-
+// CELL - DOCUMENT . CLASS . TS
 import { Content } from "./content.class";
 import { Model } from "./model.class";
 
@@ -26,6 +11,7 @@ content?: Content;
 
 constructor(public name: string,
 			public uri: string,
+			public desc: string,
 			public kind: string,
 			public modelURI: string,
 			public contentURI: string,
@@ -76,6 +62,7 @@ export interface CellDocumentJSON {
 	
 name: string;
 uri: string;
+desc: string;
 kind: string;
 modelURI: string;
 contentURI: string;
@@ -84,3 +71,19 @@ problem: string;
 presentation?: string;
 
 }
+
+/*
+ *	  Copyright 2017 Daniel Giribet
+ *
+ *	 Licensed under the Apache License, Version 2.0 (the "License");
+ *	 you may not use this file except in compliance with the License.
+ *	 You may obtain a copy of the License at
+ *
+ *		 http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *	 Unless required by applicable law or agreed to in writing, software
+ *	 distributed under the License is distributed on an "AS IS" BASIS,
+ *	 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *	 See the License for the specific language governing permissions and
+ *	 limitations under the License.
+ */
