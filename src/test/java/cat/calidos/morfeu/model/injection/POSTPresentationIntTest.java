1484bea5e0ffce8a2c8b0caac5f7a35ff4b8bdfb
Daniel Giribet <dani@calidos.cat>
2019-10-29 00:50:27 +0100

    POST presentation test

---
+++ src/test/java/cat/calidos/morfeu/model/injection/POSTPresentationIntTest.java
@@ -0,0 +1,53 @@
+package cat.calidos.morfeu.model.injection;
+
+import static org.junit.Assert.*;
+
+import java.io.InputStream;
+import java.util.List;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import cat.calidos.morfeu.api.APITezt;
+import cat.calidos.morfeu.utils.Config;
+import cat.calidos.morfeu.utils.MorfeuUtils;
+
+/**
+*	@author daniel giribet
+*///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+public class POSTPresentationIntTest extends APITezt {
+
+
+@Test //@DisplayName("Testing post presentation")
+public void testPOSTPresentation() throws Exception {
+
+	InputStream answerStream = postToRemote("preview/html/","text=text%20here?color=00ff0a");
+	assertNotNull(answerStream);
+	String answer = IOUtils.toString(answerStream, Config.DEFAULT_CHARSET);
+	System.err.println(answer);
+	assertTrue(answer.contains("#00ff0a"));
+	assertTrue(answer.contains("text here"));
+
+}
+
+
+}
+
+/*
+ *    Copyright 2019 Daniel Giribet
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
