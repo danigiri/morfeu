50ab5e213078e5286ac59d1d9b8f8c22a47ea9bc
Daniel Giribet <dani@calidos.cat>
2020-01-02 13:53:59 +0100

    ncurses for pretty output

--- src/main/scripts/angular.sh
+++ src/main/scripts/angular.sh
@@ -6,10 +6,10 @@ normal=$(tput sgr0)
 
 if [ -d ${project.basedir}/src/main/angular ]; then
 	cd ${project.basedir}/src/main/angular
-	printf "${GREEN}%s${normal}" 'Starting Angular build...'
+	printf "${GREEN}%s${normal}\n" 'Starting Angular build...'
 	ng build --prod --optimization=true --aot=true --deleteOutputPath=false --outputPath=${project.build.directory}/dist
-	printf "${GREEN}%s${normal}" 'Angular build finished'
+	printf "${GREEN}%s${normal}\n" 'Angular build finished'
 else
-	printf "${RED}%s${normal}" 'FOLDER ${project.basedir}/src/main/angular DOES NOT EXIST'
+	printf "${RED}%s${normal}\n" 'FOLDER ${project.basedir}/src/main/angular DOES NOT EXIST'
 	exit -1
 fi
