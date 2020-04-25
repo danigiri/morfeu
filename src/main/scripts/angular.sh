619531b988f3226153c77ebd911485b6e55db2ff
Daniel Giribet <dani@calidos.cat>
2020-04-25 23:31:13 +0200

    before angular update

--- src/main/scripts/angular.sh
+++ src/main/scripts/angular.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
+#exit 0
+
 GREEN=$(tput setaf 2)
 RED=$(tput setaf 1)
-normal=$(tput sgr0)
+NORMAL=$(tput sgr0)
 
 if [ -d ${project.basedir}/src/main/angular ]; then
 	cd ${project.basedir}/src/main/angular
 	printf "${GREEN}%s${normal}\n" 'Starting Angular build...'
 	npm install --no-optional
 	ng build --prod --optimization=true --aot=true --deleteOutputPath=false --outputPath=${project.build.directory}/dist
-	printf "${GREEN}%s${normal}\n" 'Angular build finished'
+	printf "${GREEN}%s${NORMAL}\n" 'Angular build finished'
 else
-	printf "${RED}%s${normal}\n" 'FOLDER ${project.basedir}/src/main/angular DOES NOT EXIST'
+	printf "${RED}%s${NORMAL}\n" 'FOLDER ${project.basedir}/src/main/angular DOES NOT EXIST'
 	exit -1
 fi
