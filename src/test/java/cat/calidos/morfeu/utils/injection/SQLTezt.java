f785d61a836230b37f6713b3075491c860b61aca
Daniel Giribet <dani@calidos.cat>
2020-08-12 16:01:35 +0200

    fix concurrency database test failure

--- src/test/java/cat/calidos/morfeu/utils/injection/SQLTezt.java
+++ src/test/java/cat/calidos/morfeu/utils/injection/SQLTezt.java
@@ -1,26 +1,35 @@
 package cat.calidos.morfeu.utils.injection;
 
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
 import cat.calidos.morfeu.problems.ParsingException;
+import cat.calidos.morfeu.utils.Tezt;
 
 
-public class SQLTezt {
+public class SQLTezt extends Tezt {
 
 protected Connection connection;
 protected Statement statement;
+private String databasePath;
 
 
 @BeforeEach
 public void setupDatabase() throws Exception {
 
+	File databaseDirectory = setupTempDirectory();
+	// we may have concurrency issues with the database, we skip testing then
 	org.h2.Driver.load();
-	connection = DriverManager.getConnection("jdbc:h2:~/test", "sa", "sa");
+	databasePath = databaseDirectory+"/database";
+	FileUtils.forceMkdir(new File (databasePath));
+	System.err.println("Using database temp path "+databasePath);
+	connection = DriverManager.getConnection("jdbc:h2:"+databasePath+"/foo", "sa", "sa");
 	statement = SQLModule.statement(connection);
 
 }
@@ -36,6 +45,10 @@ public void teardown() throws Exception {
 	if (connection!=null) {
 		connection.close();
 	}
+
+//	if (!databasePath.isEmpty()) {
+//		FileUtils.deleteQuietly(new File (databasePath));
+//	}
 
 }
 
