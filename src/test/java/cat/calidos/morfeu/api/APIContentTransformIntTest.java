298eaf18ab97836680534e5b2b58b2822b0361a1
Daniel Giribet <dani@calidos.cat>
2021-01-03 16:34:02 +0100

    correct empty name at root

--- src/test/java/cat/calidos/morfeu/api/APIContentTransformIntTest.java
+++ src/test/java/cat/calidos/morfeu/api/APIContentTransformIntTest.java
@@ -26,7 +26,7 @@ public void testDocument1YAMLTransform() throws Exception {
 	JsonNode root = parseJson(content);							// <root node>
 	assertNotNull(root);
 	assertEquals("Wrong document schema", 0, root.get("schema").asInt());
-	assertEquals("Root node has a wrong name", "document1.yaml", root.get("name").asText());
+	assertEquals("Root node has a wrong name", "", root.get("name").asText());
 	assertTrue("/children is not an array and it should be", root.get("children").isArray());
 
 	JsonNode test = root.get("children").get(0);				// /test(0)
