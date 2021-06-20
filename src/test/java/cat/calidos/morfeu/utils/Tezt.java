318576afc0e3ee5962057975714281eee03401f9
Daniel Giribet <dani@calidos.cat>
2021-06-20 16:12:21 +0100

    test url updated

--- src/test/java/cat/calidos/morfeu/utils/Tezt.java
+++ src/test/java/cat/calidos/morfeu/utils/Tezt.java
@@ -26,7 +26,7 @@ import org.xmlunit.diff.Diff;
 public class Tezt {
 
 protected static final String URL_PROPERTY = "app-url";
-protected static String DEFAULT_URL = "http://localhost:8980";
+protected static String DEFAULT_URL = "http://localhost:3000/";
 public static final String DEFAULT_TMP_FOLDER_NAME = "integration-tests-tmp";
 
 
