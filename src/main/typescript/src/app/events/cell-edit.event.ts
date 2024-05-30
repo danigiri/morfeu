4c624bab10e0aa8208204e202869f2a2966b6a3a
Daniel Giribet <dani@calidos.cat>
2024-05-30 12:17:54 +0200

    cell edit event to string assumed cell was defined

--- src/main/typescript/src/app/events/cell-edit.event.ts
+++ src/main/typescript/src/app/events/cell-edit.event.ts
@@ -12,14 +12,14 @@ constructor(public cell?: Cell) {
 
 
 public override toString = (): string => {
-	return "CellEditEvent:{cell:"+this.cell.URI+"}";
+	return "CellEditEvent:{cell:"+(this.cell? this.cell.URI : '(not yet defined)')+"}";
 }
 
 
 }
 
 /*
- *	  Copyright 2019 Daniel Giribet
+ *	  Copyright 2024 Daniel Giribet
  *
  *	 Licensed under the Apache License, Version 2.0 (the "License");
  *	 you may not use this file except in compliance with the License.
