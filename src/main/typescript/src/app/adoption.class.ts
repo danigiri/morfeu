2d1745f63fb6a2874bf7fe75545e01b5528ee093
Daniel Giribet <dani@calidos.cat>
2024-06-07 17:19:29 +0100

    forgot to add adoption class

--- src/main/typescript/src/app/adoption.class.ts
+++ src/main/typescript/src/app/adoption.class.ts
@@ -8,99 +8,111 @@ import { FamilyMember } from './family-member.interface';
 export class Adoption {
 
 
-public static hasCellModel(cell: Cell): boolean {
-	return cell.cellModel!==undefined; // this will be true for the root node, though this can be refactored
-}
-
-
-public static canBeModified(cell: Cell): boolean {
-	return cell.canBeModified();
-}
-
-
-public static isModelCompatible(cell: Cell, newMember: FamilyMember): boolean {
-	return cell.cellModel.canAdopt(newMember);
-}
-
-
-/** we check the allowed count (if we have no children we assume zero so we should be able to add) */
-public static weHaveRoomForOneMore(cell: Cell, newMember: FamilyMember): boolean {
-
-	// if the newMember is part of the children we always have room for one more
-	if (cell===newMember.getParent()) {
-		return true;
+	public static hasCellModel(cell: Cell): boolean {
+		return cell.cellModel !== undefined; // this will be true for the root node, though this can be refactored
 	}
 
-	const matchingChildren: Cell[] = cell.children ? cell.children.filter(c => c.matches(newMember)) : [];
-	const childCount = matchingChildren.length;
-	if (childCount>0) {
-		// we are not considering the problem of the childcount being less than the minimum
-		//TODO: add check: are we able to remove this cell as child?
-		let matchingCellModel:CellModel = matchingChildren[0].cellModel;
-		if (matchingCellModel.maxOccurs && childCount >= matchingCellModel.maxOccurs) { // notice we use '>=' as we are adding one more
-			return false;
+
+	public static canBeModified(cell: Cell): boolean {
+		return cell.canBeModified();
+	}
+
+
+	public static isModelCompatible(cell: Cell, newMember: FamilyMember): boolean {
+		return cell.cellModel.canAdopt(newMember);
+	}
+
+
+	/** we check the allowed count (if we have no children we assume zero so we should be able to add) */
+	public static weHaveRoomForOneMore(cell: Cell, newMember: FamilyMember): boolean {
+
+		// if the newMember is part of the children we always have room for one more
+		if (cell === newMember.getParent()) {
+			return true;
 		}
-	}
 
-	return true;
-}
+		const matchingChildren: Cell[] = cell.children ? cell.children.filter(c => c.matches(newMember)) : [];
+		const childCount = matchingChildren.length;
+		if (childCount > 0) {
+			// we are not considering the problem of the childcount being less than the minimum
+			//TODO: add check: are we able to remove this cell as child?
+			let matchingCellModel: CellModel = matchingChildren[0].cellModel;
+			if (matchingCellModel.maxOccurs && childCount >= matchingCellModel.maxOccurs) { // notice we use '>=' as we are adding one more
+				return false;
+			}
+		}
 
-
-
-/** we check that if we are trying to add into adjacent positions, this is a noop we want to avoid
-	place, example:
-	<col>
-		[drop area 0]
-		<thingie/>
-		[drop area 1]
-		<otherthing/>
-		[drop area 2]
-	</col>
-	in this case, <thingie/> does not make sense to activate drop areas 0 and 1 as cell ends up the same
-*/
-public static isNotAdjacentPosition(cell: Cell, newMember: FamilyMember, position: number): boolean {
-
-	if (cell.children===undefined ||  cell.children.length===0 || !cell.equals(newMember.getParent())) {
 		return true;
 	}
-	const child = cell.children.find(c => c.getURI()===newMember.getURI());
-
-	return position!=child.position && position!=child.position+1;	
-	//return true;
-
-}
 
 
-public static itsTheRightOrder(cell: Cell, newMember: FamilyMember, position: number): boolean {
 
-	let ok = false;
+	/** we check that if we are trying to add into adjacent positions, this is a noop we want to avoid
+		place, example:
+		<col>
+			[drop area 0]
+			<thingie/>
+			[drop area 1]
+			<otherthing/>
+			[drop area 2]
+		</col>
+		in this case, <thingie/> does not make sense to activate drop areas 0 and 1 as cell ends up the same
+	*/
+	public static isNotAdjacentPosition(cell: Cell, newMember: FamilyMember, position: number): boolean {
 
-	const childrenCount = cell.childrenCount();
-	const model = cell.cellModel;
-	if (childrenCount>0 && model.children!==undefined && model.areChildrenOrdered) {
+		// WARNING: in the case of a snippet, get parent is null, in this case the cell equivalence is automatically false
+		// it's not clear if this is 100% the right behaviour, but it matches the semantics of this adjacency test
+		if (cell.children === undefined || cell.children.length === 0 || !cell.equals(newMember.getParent())) {
+			return true;
+		}
+		const child = cell.children.find(c => c.getURI() === newMember.getURI());
 
-		const adoptionOrder = newMember.getAdoptionOrder();
-		if (position===0) {						// at the beginning, check first cell
-			ok = (adoptionOrder <= cell.children[0].getAdoptionOrder());
-		} else if (position===childrenCount) {	// at the end, check last cell
-			ok = cell.children[cell.childrenCount()-1].getAdoptionOrder() <= adoptionOrder;
-		} else {								// in the middle, check previous and next cells
-			ok = (cell.children[position-1].getAdoptionOrder() <= adoptionOrder) &&
+		return position != child.position && position != child.position + 1;
+		//return true;
+
+	}
+
+
+	public static itsTheRightOrder(cell: Cell, newMember: FamilyMember, position: number): boolean {
+
+		let ok = false;
+
+		const childrenCount = cell.childrenCount();
+		const model = cell.cellModel;
+		if (childrenCount > 0 && model.children !== undefined && model.areChildrenOrdered) {
+
+			const adoptionOrder = newMember.getAdoptionOrder();
+			if (position === 0) {						// at the beginning, check first cell
+				ok = (adoptionOrder <= cell.children[0].getAdoptionOrder());
+			} else if (position === childrenCount) {	// at the end, check last cell
+				ok = cell.children[cell.childrenCount() - 1].getAdoptionOrder() <= adoptionOrder;
+			} else {								// in the middle, check previous and next cells
+				ok = (cell.children[position - 1].getAdoptionOrder() <= adoptionOrder) &&
 					(adoptionOrder <= cell.children[position].getAdoptionOrder());
+			}
+
+		} else {
+			ok = true;
 		}
 
-	} else {
-		ok = true;
+		return ok;
 	}
 
-	return ok;	
-}
 
-
+	public static adoptionMapToString(a: Map<string,boolean>): string {
+		if (a==null) {
+			return null;
+		}
+		let out: string[] = [];
+		for (const k of a.keys()) {
+			out.push(k+':'+(a.get(k) ? 'true' : 'false'));
+		}
+		return out.join(',');
+	}
 }
 
 /*
- *	  Copyright 2020 Daniel Giribet
+ *	  Copyright 2024 Daniel Giribet
  *
  *	 Licensed under the Apache License, Version 2.0 (the "License");
  *	 you may not use this file except in compliance with the License.
