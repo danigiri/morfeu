a701a9c8ac71c4b28b732ecbc7aae8703fd3cbaa
Daniel Giribet <dani@calidos.cat>
2024-07-22 19:06:00 +0200

    fixed browser issue, was being appended by ng build

--- src/main/scripts/angular.sh
+++ src/main/scripts/angular.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #exit 0
-if [ -v $TERM ]; then
+if [ -v TERM ]; then
 	GREEN=$(tput setaf 2)
 	RED=$(tput setaf 1)
 	NORMAL=$(tput sgr0)
@@ -19,21 +19,11 @@ if [ -d ${project.basedir}/src/main/typescript ]; then
 	# npm install --no-optional --force
 	npm install --force
 	# bundle analysis https://www.npmjs.com/package/webpack-bundle-analyzer
-	ng build --configuration production --optimization=true --aot=true --delete-output-path=false --output-path=${project.build.directory}/dist
+	# be aware that ng build appends a /browser to the output path, we will solve this in the war building
+	# it is also **relative**, so we go from src/main/typescript down and then go to target, which is hardcoded
+	# unfortunately, it looks like ng build does not support absolute paths anymore
+	ng build --configuration production --optimization=true --aot=true --delete-output-path=false --output-path=../../../target/dist
 	if [ $? -eq 0 ]; then
-		# looks like in the argo build environment, we are outputting to the 'browser' folder? adding mitigation
-		if [ -d ${project.build.directory}/dist ]; then
-			printf "${GREEN}%s${NORMAL}\n" 'target dist folder exists as expected'
-		else
-			printf "${RED}%s '%s' %s${NORMAL}\n" 'target' ${project.build.directory}/dist 'not found, trying alternative folder'
-			if [ -d ${project.build.directory}/browser ]; then
-				printf "${GREEN}%s${NORMAL}\n" 'moving generated assets from browser folder to dist'
-				mv ${project.build.directory}/browser/* ${project.build.directory}/dist/
-			else
-				printf "${RED}%s${NORMAL}\n" 'NG BUILD DID NOT WORK (no dist and no browser fallback folder)'
-				exit -1
-			fi
-		fi
 		printf "${GREEN}%s${NORMAL}\n" 'Angular build finished'
 	else
 		printf "${RED}%s${NORMAL}\n" 'NG BUILD DID NOT WORK (ng build failed)'
