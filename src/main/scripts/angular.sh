d37e3a11626567d4aac4fd2f8c3082fda2e5ca8e
Daniel Giribet <dani@calidos.cat>
2024-07-22 16:51:57 +0200

    workaround for different dist folder in k8s argo build

--- src/main/scripts/angular.sh
+++ src/main/scripts/angular.sh
@@ -15,9 +15,22 @@ if [ -d ${project.basedir}/src/main/typescript ]; then
 	# bundle analysis https://www.npmjs.com/package/webpack-bundle-analyzer
 	ng build --configuration production --optimization=true --aot=true --delete-output-path=false --output-path=${project.build.directory}/dist
 	if [ $? -eq 0 ]; then
+		# looks like in the argo build environment, we are outputting to the 'browser' folder?
+		if [ -d ${project.build.directory}/dist ]; then
+			printf "${GREEN}%s${NORMAL}\n" 'target dist folder exists as expected'
+		else
+			printf "${RED}%s '%s' %s${NORMAL}\n" 'target' ${project.build.directory}/dist 'not found, trying alternative folder'
+			if [ -d ${project.build.directory}/browser ]; then
+				printf "${GREEN}%s${NORMAL}\n" 'moving generated assets from browser folder to dist'
+				mv ${project.build.directory}/browser/* ${project.build.directory}/dist/
+			else
+				printf "${RED}%s${NORMAL}\n" 'NG BUILD DID NOT WORK (no dist and no browser fallback folder)'
+				exit -1
+			fi
+		fi
 		printf "${GREEN}%s${NORMAL}\n" 'Angular build finished'
 	else
-		printf "${RED}%s${NORMAL}\n" 'NG BUILD DID NOT WORK'
+		printf "${RED}%s${NORMAL}\n" 'NG BUILD DID NOT WORK (ng build failed)'
 		exit -1
 	fi
 else
