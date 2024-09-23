037a8cfe765b00337dacad2c401f803167ea6df5
Daniel Giribet <dani@calidos.cat>
2024-09-23 18:32:40 +0200

    morfeu proxy now can use templating

--- src/main/java/cat/calidos/morfeu/proxy/MorfeuProxyServlet.java
+++ src/main/java/cat/calidos/morfeu/proxy/MorfeuProxyServlet.java
@@ -11,6 +11,7 @@ import org.slf4j.LoggerFactory;
 import cat.calidos.morfeu.utils.injection.DaggerConfigPropertyComponent;
 import cat.calidos.morfeu.view.injection.DaggerViewComponent;
 import cat.calidos.morfeu.webapp.injection.DaggerServletConfigComponent;
+import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 
 
@@ -39,6 +40,19 @@ protected void initTarget() throws ServletException {
 	}
 
 	targetUri = getConfigParam(P_TARGET_URI);
+	targetUri = MorfeuProxyServlet.getFinalTargetURI(targetUri, this.getServletConfig());
+
+	try {
+		targetUriObj = new URI(targetUri);
+	} catch (Exception e2) {
+		throw new ServletException("Trying to process targetUri init parameter: " + e2);
+	}
+	targetHost = URIUtils.extractHost(targetUriObj);
+
+}
+
+
+public static String getFinalTargetURI(String targetUri, ServletConfig config) throws ServletException {
 	targetUri = DaggerConfigPropertyComponent.builder()
 									.forName(P_TARGET_URI)
 									.allowEmpty(false)
@@ -55,43 +69,34 @@ protected void initTarget() throws ServletException {
 		// template snippet in the param, so we populate the view with the servlet config, system vars and env which
 		// can be used in the param to provide advanced features (like variable substitution)
 		Properties configuration = DaggerServletConfigComponent.builder()
-									.with(this.getServletConfig())
-									.build()
-									.getProperties();
+																.with(config)
+																.build()
+																.getProperties();
 		log.trace("(From configuration) __PROXY_PREFIX='{}'",configuration.getProperty(__PROXY_PREFIX, "not set!"));
 		targetUri = DaggerViewComponent.builder()
-				.withValue(configuration)
+				.withValues(configuration)
 				.withTemplate(targetUri)
 				.build()
 				.render();
 		log.info("** Using template-based {} for proxy target uri", targetUri);
 	}
-
-	try {
-		targetUriObj = new URI(targetUri);
-	} catch (Exception e2) {
-		throw new ServletException("Trying to process targetUri init parameter: " + e2);
-	}
-	targetHost = URIUtils.extractHost(targetUriObj);
-
+	return targetUri;
 }
 
 
 }
 
 /*
- *    Copyright 2024 Daniel Giribet
+ * Copyright 2024 Daniel Giribet
  *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 
---
+++ src/test/java/cat/calidos/morfeu/proxy/MorfeuProxyServletTest.java
@@ -0,0 +1,40 @@
+package cat.calidos.morfeu.proxy;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import jakarta.servlet.ServletException;
+
+public class MorfeuProxyServletTest {
+
+
+@Test @DisplayName("Test get target uri config")
+public void testGetFinalTargetURI() throws ServletException {
+
+	//assertEquals("foo", MorfeuProxyServlet.getFinalTargetURI("foo", null));
+
+	System.setProperty(MorfeuProxyServlet.__PROXY_PREFIX, "foo");
+	var targetUri = "${"+MorfeuProxyServlet.__PROXY_PREFIX+"}";
+	assertEquals("foo", MorfeuProxyServlet.getFinalTargetURI(targetUri, null));
+
+}
+
+
+}
+
+/*
+ * Copyright 2024 Daniel Giribet
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
