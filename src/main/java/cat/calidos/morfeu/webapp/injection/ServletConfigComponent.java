77c2d12686264ca4a5dc7f12e93ac93c54b0fd77
Daniel Giribet <dani@calidos.cat>
2024-09-23 18:32:02 +0200

    more streamlined injection in servlet config

--- src/main/java/cat/calidos/morfeu/webapp/injection/ServletConfigComponent.java
+++ src/main/java/cat/calidos/morfeu/webapp/injection/ServletConfigComponent.java
@@ -4,8 +4,11 @@ package cat.calidos.morfeu.webapp.injection;
 
 import java.util.Properties;
 
+import javax.annotation.Nullable;
+
 import dagger.BindsInstance;
 import dagger.Component;
+
 import jakarta.servlet.ServletConfig;
 
 /**
@@ -21,7 +24,7 @@ Properties getProperties();
 @Component.Builder
 interface Builder {
 
-	@BindsInstance Builder with(ServletConfig c);
+	@BindsInstance Builder with(@Nullable ServletConfig c); // will use empty configuration if not set
 
 	ServletConfigComponent build();
 
@@ -30,17 +33,15 @@ interface Builder {
 }
 
 /*
- *    Copyright 2016 Daniel Giribet
+ * Copyright 2016 Daniel Giribet
  *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
--- src/main/java/cat/calidos/morfeu/webapp/injection/ServletConfigModule.java
+++ src/main/java/cat/calidos/morfeu/webapp/injection/ServletConfigModule.java
@@ -2,9 +2,17 @@
 
 package cat.calidos.morfeu.webapp.injection;
 
+import static org.mockito.Mockito.CALLS_REAL_METHODS;
+
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
 import java.util.Properties;
 import java.util.stream.StreamSupport;
+
+import javax.annotation.Nullable;
 
 import dagger.Module;
 import dagger.Provides;
@@ -19,41 +27,66 @@ public class ServletConfigModule {
 
 
 @Provides
-public static Properties getProperties(ServletConfig c) {
+public static Properties getProperties(@Nullable ServletConfig config,
+										Optional<ServletContext> servletContext,
+										List<String> parameterNames, 
+										Iterable<String> attributeNames) {
 
 	Properties p = new Properties();
-	Collections.list(c.getInitParameterNames()).forEach(name -> p.setProperty(name, c.getInitParameter(name)));
+	if (config !=null) {
+		parameterNames.forEach(name -> p.setProperty(name, config.getInitParameter(name)));
+	}
 
-	ServletContext context = c.getServletContext();
-	Iterable<String> iterable = () -> context.getAttributeNames().asIterator();
-	StreamSupport.stream(iterable.spliterator(), false)
-					.filter(a -> context.getAttribute(a) instanceof String)
-					.forEach(a -> p.setProperty(a, (String)context.getAttribute(a)));
+	if (servletContext.isPresent()) {
+		ServletContext context = servletContext.get();
+		StreamSupport.stream(attributeNames.spliterator(), false)
+						.filter(a -> context.getAttribute(a) instanceof String)
+						.forEach(a -> p.setProperty(a, (String)context.getAttribute(a)));
+	}
 
 	p.putAll(System.getProperties());
 	p.putAll(System.getenv());
-
-	//p.propertyNames().asIterator().forEachRemaining(null);;
 
 	return p;
 
 }
 
 
+@Provides
+public static List<String> parameterNames(@Nullable ServletConfig c) {
+	return c==null ?
+			List.of()
+	: Collections.list(c.getInitParameterNames());
+}
+
+
+@Provides
+public static Iterable<String> attributeNames(@Nullable ServletConfig c) {
+	if (c==null) {
+		return () -> new ArrayList<String>().iterator();
+	}
+	ServletContext context = c.getServletContext();
+	return () -> context.getAttributeNames().asIterator();
+}
+
+
+@Provides
+public static Optional<ServletContext> servletContext(@Nullable ServletConfig c) {
+	return Optional.ofNullable(c == null ? null : c.getServletContext());
+}
+
 }
 
 /*
- *    Copyright 2024 Daniel Giribet
+ * Copyright 2024 Daniel Giribet
  *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
